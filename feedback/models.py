dbc5403251e8c8c1c3caac915d8bd9a548ff1f2d
Aaquib111 <aaquibahm@gmail.com>
2019-12-28 18:14:29 -0500

    commit

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    first_name = models.CharField(verbose_name="First Name")
+    last_name = models.CharField(verbose_name="Last Name")
+    message = models.TextField(verbose_name="Message", max_length=100)
