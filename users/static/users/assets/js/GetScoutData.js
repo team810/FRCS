76f69caf3856a2315e152dcfbb9d2a459b58785f
JackPatt1122 <jtyler03@optonline.net>
2020-01-08 17:23:35 -0800

    added comp peram

--- users/static/users/assets/js/GetScoutData.js
+++ users/static/users/assets/js/GetScoutData.js
@@ -5,27 +5,26 @@ window.onload = async function getTeamNumber(){
 	var team = prompt("Enter Team Number")
 	$("#teamNumberHeader").html(team);
 	$.get("https://www.thebluealliance.com/api/v3/team/frc" + team + "/events/2020/simple", {"X-TBA-Auth-Key": "PzOW8s1DYGlVkgAsikwVlhy5wZ5Tm85fKSjd0DfiUJFQOGhsReyZEf88EEoAU1Cw"}, function(data) {
-	var jObject = (data);
+    var jObject = (data);
+    console.log(data);
     for(var i=0; i<data.length; i++){
+        
     var obj = jObject[i];
+    var comp = obj.name;
+
     key = obj.event_code;
     var date = obj.start_date;
-	console.log(date)
+    console.log(date)
     console.log(key)
+    var option = "<option id='compValueDrop' value=" + key + ">" + comp + "</option>"
+    document.getElementById('getCompDrop').innerHTML += option;
     }
 })}
-
-	if(team == null){
-        $("#teamNumberHeader").html("Error");
-        
-	}
 	
 
 //Change teams in dropdown list
-
-
 async function changeTeams(){
-    var matchType = $( "#matchTypeOp option:selected" ).val();
+    var matchType = $("#matchTypeOp option:selected").val();
     var matchNumber = document.getElementById("matchNumber").value;
     var matchId = "2019nyli2_";
 
@@ -34,8 +33,7 @@ async function changeTeams(){
     var sf = "sf";
     var f = "f";
     var ef = "ef";
-
-    if(matchType === "qf"){
+if(matchType === "qf"){
         var sel = '<input class="form-control" type="number" name="teamNumbert" id="teamNumber"/>';
 
         document.getElementById('teamNumberInput').innerHTML = sel;
@@ -63,7 +61,7 @@ async function changeTeams(){
     console.log(matchType)
     $(document).ready(function () {
     document.getElementById('teamNumber').innerHTML = "";
-    $.get("https://www.thebluealliance.com/api/v3/match/" + matchId + matchType + matchNumber + "/simple", {"X-TBA-Auth-Key": "PzOW8s1DYGlVkgAsikwVlhy5wZ5Tm85fKSjd0DfiUJFQOGhsReyZEf88EEoAU1Cw"}, function(data) {    
+    $.get("https://www.thebluealliance.com/api/v3/match/2019" + matchTypeOp + "_" + matchType + matchNumber + "/simple", {"X-TBA-Auth-Key": "PzOW8s1DYGlVkgAsikwVlhy5wZ5Tm85fKSjd0DfiUJFQOGhsReyZEf88EEoAU1Cw"}, function(data) {    
     var jObject = (data);
     console.log(data);
     var alliance = jObject.alliances;
@@ -71,10 +69,13 @@ async function changeTeams(){
     var blue = alliance.blue.team_keys;
     var red = alliance.red.team_keys;
     
+    
 
     blue.forEach(function(name){
-    var option = "<option isd='teamNumber' value='" + name + "'>" + name + "</option>";
+    document.getElementById('teamNumber').innerHTML += option;
+    var option = "<option value='" + name + "'>" + name + "</option>"
     document.getElementById('teamNumber').innerHTML += option; 
+
         })
 
     red.forEach(function(name){
@@ -96,4 +97,10 @@ async function changeName(){
     document.getElementById("teamName").innerHTML = name;
     });
 });
+}
+
+
+function getCompData(){
+ matchTypeOp = $("#getCompDrop option:selected").val();
+ console.log(matchTypeOp);
 }
