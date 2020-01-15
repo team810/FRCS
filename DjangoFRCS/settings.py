23fc598683a79070c6dd810a89f40460f6e36448
JackPatt1122 <jtyler03@optonline.net>
2020-01-14 18:51:31 -0800

    smtp finally working

--- DjangoFRCS/settings.py
+++ DjangoFRCS/settings.py
@@ -132,6 +132,7 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = 'login-view'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.google.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
--- users/views.py
+++ users/views.py
@@ -10,10 +10,14 @@ from users.forms import UserCreationForm, UserLoginForm, UserChangeForm
 from users.models import CustomUser
 from teams.models import Team
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
 from django.template import loader
-from django.core.mail import send_mail
 import smtplib
+from django.template.loader import get_template
+from django.template.loader import render_to_string
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
 
 #from .forms import CustomUserCreationForm
 #from .models import UserProfile
@@ -57,18 +61,234 @@ def register(request):
             username = form.cleaned_data['username']
             is_team_admin = form.cleaned_data['is_team_admin']
             team_num = form.cleaned_data['team_num']
+            email = form.cleaned_data['email']
 
-            html_message = loader.render_to_string('users/emailtemp.html')
-            msgEmail = 'users/emailtemp.html'
+            template = """<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+  <style type="text/css" rel="stylesheet" media="all">
+  
+    
+    body {
+      width: 100% !important;
+      height: 100%;
+      margin: 0;
+      line-height: 1.4;
+      background-color: #F5F7F9;
+      color: #839197;
+      -webkit-text-size-adjust: none;
+      font-family: 'poppins', 'sans-serif'
+    }
+    a {
+      color: #414EF9;
+    }
+
+    /* Layout ------------------------------ */
+    .email-wrapper {
+      width: 100%;
+      margin: 0;
+      padding: 0;
+      background-color: #F5F7F9;
+    }
+    .email-content {
+      width: 100%;
+      margin: 0;
+      padding: 0;
+    }
+
+    /* Masthead ----------------------- */
+    .email-masthead {
+      padding: 25px 0;
+      text-align: center;
+    }
+    .email-masthead_logo {
+      max-width: 400px;
+      border: 0;
+    }
+    .email-masthead_name {
+      font-size: 16px;
+      font-weight: bold;
+      color: #839197;
+      text-decoration: none;
+      text-shadow: 0 1px 0 white;
+    }
+
+    /* Body ------------------------------ */
+    .email-body {
+      width: 100%;
+      margin: 0;
+      padding: 0;
+      border-top: 1px solid #E7EAEC;
+      border-bottom: 1px solid #E7EAEC;
+      background-color: #FFFFFF;
+    }
+    .email-body_inner {
+      width: 570px;
+      margin: 0 auto;
+      padding: 0;
+    }
+    .email-footer {
+      width: 570px;
+      margin: 0 auto;
+      padding: 0;
+      text-align: center;
+    }
+    .email-footer p {
+      color: #839197;
+    }
+    .body-action {
+      width: 100%;
+      margin: 30px auto;
+      padding: 0;
+      text-align: center;
+    }
+    .body-sub {
+      margin-top: 25px;
+      padding-top: 25px;
+      border-top: 1px solid #E7EAEC;
+    }
+    .content-cell {
+      padding: 35px;
+    }
+    .align-right {
+      text-align: right;
+    }
+
+    /* Type ------------------------------ */
+    h1 {
+      margin-top: 0;
+      color: #292E31;
+      font-size: 19px;
+      font-weight: bold;
+      text-align: left;
+    }
+    h2 {
+      margin-top: 0;
+      color: #292E31;
+      font-size: 16px;
+      font-weight: bold;
+      text-align: left;
+    }
+    h3 {
+      margin-top: 0;
+      color: #292E31;
+      font-size: 14px;
+      font-weight: bold;
+      text-align: left;
+    }
+    p {
+      margin-top: 0;
+      color: #839197;
+      font-size: 16px;
+      line-height: 1.5em;
+      text-align: left;
+    }
+    p.sub {
+      font-size: 12px;
+    }
+    p.center {
+      text-align: center;
+    }
+
+    /* Buttons ------------------------------ */
+    .button {
+      display: inline-block;
+      width: 200px;
+      background-color: lightblue;
+      border-radius: 5px;
+      color: #ffffff;
+      font-size: 15px;
+      line-height: 45px;
+      text-align: center;
+      font-weight: bold;
+      text-decoration: none;
+      -webkit-text-size-adjust: none;
+      mso-hide: all;
+      border-color: transparent;
+    }
+
+    /*Media Queries ------------------------------ */
+    @media only screen and (max-width: 600px) {
+      .email-body_inner,
+      .email-footer {
+        width: 100% !important;
+      }
+    }
+    @media only screen and (max-width: 500px) {
+      .button {
+        width: 100% !important;
+      }
+    }
+
+    #title{
+        font-size: 40px;
+    }
+  </style>
+</head>
+<body>
+
+  <table class="email-wrapper" width="100%" cellpadding="0" cellspacing="0">
+    <tr>
+      <td align="center">
+        <table class="email-content" width="100%" cellpadding="0" cellspacing="0">
+          <!-- Logo -->
+          <tr>
+            <td class="email-masthead">
+            </td>
+          </tr>
+          <!-- Email Body -->
+          <tr>
+            <td class="email-body" width="100%">
+              <table class="email-body_inner" align="center" width="570" cellpadding="0" cellspacing="0">
+                <!-- Body content -->
+                <tr>
+                  <td class="content-cell">
+                  <h1 id="title"><strong>FRCS</strong></h1>
+                    <h1>Verify your email address</h1>
+                    <p>Thanks for signing up for FRCS! We're excited to have you as a user.</p>
+                    <!-- Action -->
+                    <table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0">
+                      <tr>
+                        <td align="center">
+                          <div>
+                            <form action="frcs.online/verify">
+                              <button class="button" type="submit">Click me</button>
+                            </form>
+                          </div>
+                        </td>
+                      </tr>
+                    </table>
+                    <p>Thanks,<br>The FRCS Team</p>
+                    <!-- Sub copy -->
+                    <table class="body-sub">
+                      <tr>
+                        <td>
+                          <p class="sub">If you’re having trouble clicking the button, copy and paste the URL below into your web browser.
+                          </p>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+             
+      </td>
+    </tr>
+  </table>
+</body>
+</html>"""
+            msg = MIMEMultipart('alternative')
+            htmly = MIMEText(template, 'html')
+            msg.attach(htmly)
             
             with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
                 smtp.ehlo()
                 smtp.starttls()
                 smtp.ehlo()
                 smtp.login("frcsassistant@gmail.com", "zikpniouyggoqfmk")
-                html_message = loader.render_to_string('users/emailtemp.html')
-                msg = msgEmail
-                smtp.sendmail('frcsassistant@gmail.com','frcsassistant@gmail.com', msg)          
+                smtp.sendmail('frcsassistant@gmail.com','frcsassistant@gmail.com', msg.as_string())          
 
             #send_mail_to = form.cleaned_data['email']
             user_obj = form.save()
@@ -115,7 +335,7 @@ def gamedata(request):
 
 @login_required
 def welcome(request):
-    html_message = loader.render_to_string('users/emailtemp.html')
+    html_message = loader.render_to_string('users/email.html')
     return render(request, 'users/welcome.html')
 
 @login_required
