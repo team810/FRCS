6635fda791a31f55fe3003e2545b702e81e6872d
Aaquib111 <aaquibahm@gmail.com>
2020-03-08 16:32:48 -0400

    static

--- .history/DjangoFRCS/settings_20200308163206.py
+++ .history/DjangoFRCS/settings_20200308163206.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'b*&l^vihv&ew@7hek!$8_^gj4flqv-w*koi^vwm-cy-g4_vab9'
 
-# SECURITY WARNING: don't run with debug tuned on in production!
-DEBUG = False
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ['localhost']
